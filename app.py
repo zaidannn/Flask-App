f24bdc2e31f7febb6215ae31f9e6337291cc0e99
Zaidann <96198627+zaidannn@users.noreply.github.com>
2025-08-30 01:21:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, DevOps World! 🚀"
+    return "Hello, DevOps World! ini update"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
